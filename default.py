99ff5de8dfa1eb00b74019df90aaee56942328ba
phil65 <philipptemminghoff@googlemail.com>
2013-03-10 05:50:49 +0100

    moved json_dumps out of edit_label

--- default.py
+++ default.py
@@ -127,55 +127,55 @@ class Main:
         if Edit_Selection == -1 :
             return
         elif Edit_Selection == 0 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.Title')),self.TYPE,"title")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.Title'))),self.TYPE,"title")
         elif Edit_Selection == 1 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.OriginalTitle')),self.TYPE,"originaltitle")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.OriginalTitle'))),self.TYPE,"originaltitle")
         elif Edit_Selection == 2 :
             dialog = xbmcgui.Dialog()
             self._edit_label(dialog.numeric(2013, xbmc.getLocalizedString(16028)),self.TYPE,"year")
         elif Edit_Selection == 3 :
             genrestring = self._set_string(xbmc.getInfoLabel('ListItem.Genre'))
             genrelist = genrestring.split( ' / ' )
-            self._edit_label(genrelist,self.TYPE,"genre")
+            self._edit_label(json.dumps(genrelist),self.TYPE,"genre")
         elif Edit_Selection == 4 :
             writerstring = self._set_string(xbmc.getInfoLabel('ListItem.Writer'))
             writerlist = writerstring.split( ' / ' )
-            self._edit_label(writerlist,self.TYPE,"writer")
+            self._edit_label(json.dumps(writerlist),self.TYPE,"writer")
         elif Edit_Selection == 5 :
             directorstring = self._set_string(xbmc.getInfoLabel('ListItem.Director'))
             directorlist = directorstring.split( ' / ' )
-            self._edit_label(directorlist,self.TYPE,"director")
+            self._edit_label(json.dumps(directorlist),self.TYPE,"director")
         elif Edit_Selection == 6 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.Tagline')),self.TYPE,"tagline")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.Tagline'))),self.TYPE,"tagline")
         elif Edit_Selection == 7 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.Plot')),self.TYPE,"plot")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.Plot'))),self.TYPE,"plot")
         elif Edit_Selection == 8 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.PlotOutline')),self.TYPE,"plotoutline")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.PlotOutline'))),self.TYPE,"plotoutline")
         elif Edit_Selection == 9 :
             dialog = xbmcgui.Dialog()
             self._edit_label(dialog.numeric(0, xbmc.getLocalizedString(16028)),self.TYPE,"top250")
         elif Edit_Selection == 10 :
-            self._edit_label(self._set_string(""),self.TYPE,"trailer")
+            self._edit_label(json.dumps(self._set_string("")),self.TYPE,"set")
         elif Edit_Selection == 11 :
             tagstring = self._set_string("")
             taglist = tagstring.split( ' / ' )
-            self._edit_label(taglist,self.TYPE,"tag")
+            self._edit_label(json.dumps(taglist),self.TYPE,"tag")
         elif Edit_Selection == 12 :
             countrystring = self._set_string(xbmc.getInfoLabel('ListItem.Country'))
             countrylist = countrystring.split( ' / ' )
-            self._edit_label(countrylist,self.TYPE,"country")
+            self._edit_label(json.dumps(countrylist),self.TYPE,"country")
         elif Edit_Selection == 13 :
             studiostring = self._set_string(xbmc.getInfoLabel('ListItem.Studio'))
             studiolist = studiostring.split( ' / ' )
-            self._edit_label(studiolist,self.TYPE,"studio")
+            self._edit_label(json.dumps(studiolist),self.TYPE,"studio")
         elif Edit_Selection == 14 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.Mpaa')),self.TYPE,"mpaa")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.Mpaa'))),self.TYPE,"mpaa")
         elif Edit_Selection == 15 :
-            self._edit_label(self._set_string(xbmc.getInfoLabel('ListItem.Trailer')),self.TYPE,"trailer")
+            self._edit_label(json.dumps(self._set_string(xbmc.getInfoLabel('ListItem.Trailer'))),self.TYPE,"trailer")
         elif Edit_Selection == 16 :
             showlinkstring = self._set_string("")
             showlinklist = showlinkstring.split( ' / ' )
-            self._edit_label(showlinklist,self.TYPE,"showlink")
+            self._edit_label(json.dumps(showlinklist),self.TYPE,"showlink")
         elif Edit_Selection == 17 :
             dialog = xbmcgui.Dialog()
             self._edit_label(dialog.numeric(0, xbmc.getLocalizedString(16028)),self.TYPE,"playcount")
@@ -195,7 +195,7 @@ class Main:
         return xbmc.getInfoLabel('Skin.String(Value)')
                     
     def _edit_label( self,genre,type,label ):
-        xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id": 1, "method": "VideoLibrary.Set%sDetails", "params": { "%s": %s, "%sid":%s }}' % (type,label,json.dumps(genre),type.lower(),self.DBID))
+        xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id": 1, "method": "VideoLibrary.Set%sDetails", "params": { "%s": %s, "%sid":%s }}' % (type,label,genre,type.lower(),self.DBID))
         
 if ( __name__ == "__main__" ):
     Main()
